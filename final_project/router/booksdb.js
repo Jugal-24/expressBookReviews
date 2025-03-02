3105418d36b285a7d6b50047737b97bcbdf2ddfe
Jugal Patidar <jugalpatidar@Jugals-MacBook-Pro.local>
2025-03-02 22:19:41 +0530

    complete the final project

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,25 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
-}
+      "1": {"author": "Chinua Achebe", "title": "Things Fall Apart", "reviews": {}},
+      "2": {"author": "Hans Christian Andersen", "title": "Fairy Tales", "reviews": {}},
+      "3": {"author": "Dante Alighieri", "title": "The Divine Comedy", "reviews": {}},
+      "4": {"author": "Ancient Mesopotamian Scribes", "title": "The Epic Of Gilgamesh", "reviews": {}},
+      "5": {"author": "Ancient Hebrew Scholars", "title": "The Book Of Job", "reviews": {}},
+      "6": {"author": "Various Middle Eastern Authors", "title": "One Thousand and One Nights", "reviews": {}},
+      "7": {"author": "Icelandic Historians", "title": "Njál's Saga", "reviews": {}},
+      "8": {"author": "Jane Austen", "title": "Pride and Prejudice", "reviews": {}},
+      "9": {"author": "Honoré de Balzac", "title": "Le Père Goriot", "reviews": {}},
+      "10": {"author": "Samuel Beckett", "title": "Molloy, Malone Dies, The Unnamable, The Trilogy", "reviews": {}},
+      "11": {"author": "George Orwell", "title": "1984", "reviews": {}},
+      "12": {"author": "Leo Tolstoy", "title": "War and Peace", "reviews": {}},
+      "13": {"author": "Fyodor Dostoevsky", "title": "Crime and Punishment", "reviews": {}},
+      "14": {"author": "Gabriel García Márquez", "title": "One Hundred Years of Solitude", "reviews": {}},
+      "15": {"author": "Homer", "title": "The Odyssey", "reviews": {}},
+      "16": {"author": "Mary Shelley", "title": "Frankenstein", "reviews": {}},
+      "17": {"author": "J.R.R. Tolkien", "title": "The Lord of the Rings", "reviews": {}},
+      "18": {"author": "Harper Lee", "title": "To Kill a Mockingbird", "reviews": {}},
+      "19": {"author": "Miguel de Cervantes", "title": "Don Quixote", "reviews": {}},
+      "20": {"author": "Virginia Woolf", "title": "To the Lighthouse", "reviews": {}}
+    }
+    
 
 module.exports=books;
